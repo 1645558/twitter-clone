989ed4f27d2864b14fffa1ecc4e1cdbc4e5ce7bc
Rare Raptor <connorwtuncc@gmail.com>
2022-09-06 13:38:58 -0400

    using state and effect to grab info from db

--- src/Feed.js
+++ src/Feed.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Feed.css';
 import TweetBox from './TweetBox';
 import Post from './Post';
 import FaceIcon from '@mui/icons-material/Face';
+import db from './firebase';
 
 function Feed() {
     const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        db.collection('posts').onSnapshot(snapshot => (
+            setPosts(snapshot.docs.map(doc => doc.data()))
+        ))
+    }, [])
     return (
         <div className='feed'>
             <div className='feed__header'>
